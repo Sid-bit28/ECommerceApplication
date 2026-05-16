aba2ea2d5ad72a42cc51ba9337d63a5b2ca0a11f
Siddarth Banerjee <siddarth2801@gmail.com>
2026-05-16 16:05:15 +0530

    initial commit, added models for Category and Product

---
+++ src/main/java/org/example/ecommerceapplication/models/Product.java
@@ -0,0 +1,24 @@
+package org.example.ecommerceapplication.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer quantity;
+    private Category category;
+    private Boolean active;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
