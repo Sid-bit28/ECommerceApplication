e38c902303ab58972deacb34e31ee5a055c854d9
Siddarth Banerjee <siddarth2801@gmail.com>
2026-05-16 16:31:43 +0530

    added validation constraints for Update Product Request

---
+++ src/main/java/org/example/ecommerceapplication/dto/ProductDTO.java
@@ -0,0 +1,16 @@
+package org.example.ecommerceapplication.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductDTO {
+    private String name;
+    private String description;
+    private Integer quantity;
+}
--- src/main/java/org/example/ecommerceapplication/dto/UpdateProductRequest.java
+++ src/main/java/org/example/ecommerceapplication/dto/UpdateProductRequest.java
@@ -1,5 +1,9 @@
 package org.example.ecommerceapplication.dto;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,9 +14,21 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class UpdateProductRequest {
+    @Size(min = 3, max = 100, message = "Name must be between 3-100 characters long")
     private String name;
+
+    @Size(min = 10, max = 1000, message = "Description must be 10-1000 characters long")
     private String description;
+
+    @DecimalMin(value = "0.01", message = "Price must be atleast $0.01")
+    @DecimalMax(value = "1000000", message = "Price cannot exceed $1000000")
     private Double price;
+
+    @Positive(message = "Quantity must be positive")
     private Integer quantity;
+
+    @Positive(message = "CategoryId must be valid")
     private Long CategoryId;
+
+    private Boolean active;
 }
