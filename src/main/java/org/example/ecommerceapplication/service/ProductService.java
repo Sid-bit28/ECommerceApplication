21fbda40f53b8ffbd8b787047df04fab09f4f8d8
Siddarth Banerjee <siddarth2801@gmail.com>
2026-05-17 01:04:38 +0530

    added pagination, dto request as argument and filters for products

--- src/main/java/org/example/ecommerceapplication/service/ProductService.java
+++ src/main/java/org/example/ecommerceapplication/service/ProductService.java
@@ -1,16 +1,21 @@
 package org.example.ecommerceapplication.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.example.ecommerceapplication.dto.CreateProductRequest;
+import org.example.ecommerceapplication.dto.UpdateProductRequest;
 import org.example.ecommerceapplication.model.Category;
 import org.example.ecommerceapplication.model.Product;
 import org.springframework.stereotype.Service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Stream;
 
 @Service
 @Slf4j
@@ -24,21 +29,20 @@ public class ProductService {
         this.categoryService = categoryService;
     }
 
-    public Product createProduct(String name, String description, Double price, Integer quantity, Long categoryId) {
-        log.info("Creating a new product with name: {}, description: {}, price: {}, quantity: {}", name, description, price, quantity);
-        Category category = categoryService.getCategoryById(categoryId);
+    public Product createProduct(CreateProductRequest request) {
+        log.info("Creating a new product with name: {}, description: {}, price: {}, quantity: {}", request.getName(), request.getDescription(), request.getPrice(), request.getQuantity());
+        Category category = categoryService.getCategoryById(request.getCategoryId());
 
         if(category == null){
-            log.error("Category with id: {} not found", categoryId);
-            return null;
+            throw new RuntimeException("Category not found");
         }
 
         Product product = Product.builder()
                 .id(productIdCounter++)
-                .name(name)
-                .description(description)
-                .price(price)
-                .quantity(quantity)
+                .name(request.getName())
+                .description(request.getDescription())
+                .price(request.getPrice())
+                .quantity(request.getQuantity())
                 .category(category)
                 .active(true)
                 .createdAt(LocalDateTime.now())
@@ -52,14 +56,37 @@ public class ProductService {
     }
 
 
-    public List<Product> getAllProducts() {
-        log.info("Fetching all products");
-        List<Product> allProducts = new ArrayList<>();
-        allProducts = productStore.values()
-                .stream()
-                .sorted(Comparator.comparing(Product::getCreatedAt).reversed())
-                .toList();
-        return allProducts;
+    public Page<Product> getAllProducts(String name, Long categoryId, Double minPrice, Double maxPrice, Boolean active, Pageable pageable) {
+        Stream<Product> stream = productStore.values().stream();
+
+        // Applying filters
+        if(name != null && !name.trim().isEmpty()) {
+            stream = stream.filter(product -> product.getName().toLowerCase().contains(name.toLowerCase()));
+        }
+
+        if(categoryId != null) {
+            stream = stream.filter(product -> product.getCategory().getId().equals(categoryId));
+        }
+
+        if(minPrice != null) {
+            stream = stream.filter(product -> product.getPrice() >= minPrice);
+        }
+
+        if(maxPrice != null) {
+            stream = stream.filter(product -> product.getPrice() <= maxPrice);
+        }
+
+        if(active != null) {
+            stream = stream.filter(product -> product.getActive() == active);
+        }
+
+        List<Product> filteredProducts = stream.sorted(Comparator.comparing(Product::getCreatedAt).reversed()).toList();
+
+        // Pagination
+        int start = (int) pageable.getOffset();
+        int end = Math.min(start + pageable.getPageSize(), filteredProducts.size());
+        List<Product> pageContent = filteredProducts.subList(start, end);
+        return new PageImpl<>(pageContent, pageable, filteredProducts.size());
     }
 
     // :TODO Create a ResourceNotFoundExceptionHandler
@@ -73,33 +100,33 @@ public class ProductService {
     }
 
     // :TODO Create a ResourceNotFoundExceptionHandler
-    public Product updateProduct(Long id, String name, String description, Double price, Integer quantity, Long CategoryId, Boolean active) {
-        log.info("Updating product with name: {}, description: {}, price: {}, quantity: {}", name, description, price, quantity);
+    public Product updateProduct(Long id, UpdateProductRequest request) {
+        log.info("Updating product with name: {}, description: {}, price: {}, quantity: {}", request.getName(), request.getDescription(), request.getPrice(), request.getQuantity());
         Product product = productStore.get(id);
         if (product == null) {
             throw new RuntimeException("Product with id " + id + " not found");
         }
-        if(CategoryId != null) {
-            Category category = categoryService.getCategoryById(CategoryId);
+        if(request.getCategoryId() != null) {
+            Category category = categoryService.getCategoryById(request.getCategoryId());
             if(category == null){
                 throw new RuntimeException("Category with id " + id + " not found");
             }
             product.setCategory(category);
         }
-        if(name != null && !name.trim().isEmpty()) {
-            product.setName(name);
+        if(request.getName() != null && !request.getName().trim().isEmpty()) {
+            product.setName(request.getName());
         }
-        if(description != null && !description.trim().isEmpty()) {
-            product.setDescription(description);
+        if(request.getDescription() != null && !request.getDescription().trim().isEmpty()) {
+            product.setDescription(request.getDescription());
         }
-        if(price != null) {
-            product.setPrice(price);
+        if(request.getPrice() != null) {
+            product.setPrice(request.getPrice());
         }
-        if(quantity != null) {
-            product.setQuantity(quantity);
+        if(request.getQuantity() != null) {
+            product.setQuantity(request.getQuantity());
         }
-        if(active != null) {
-            product.setActive(active);
+        if(request.getActive() != null) {
+            product.setActive(request.getActive());
         }
         product.setUpdatedAt(LocalDateTime.now());
         log.info("Updated product with id: {}", product.getId());
